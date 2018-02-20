6449cea33f3731af55a8203884a34a7f08cd7c0f
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-20 19:04:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 # code here!
 class School(name)
-  attr_reader :name
-
-  def initialize
-    @name = name
-  end
+  # attr_reader :name
+  #
+  # def initialize
+  #   @name = name
+  # end
 
 end
